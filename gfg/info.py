5bfb09ffb287bd13da723db97acb03afcc59e126
dalelbey <benbeydalal2019@gmail.com>
2022-05-07 17:17:01 +0200

    mon premier commit

---
+++ gfg/info.py
@@ -0,0 +1,6 @@
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'benbeydalal2019@gmail.com'
+EMAIL_HOST_PASSWORD = 'dalel14402135'
+EMAIL_PORT = 587
